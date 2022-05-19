e9be9306d597b18f0cd0c0e68149a3c8f44a0485
Eugene Kravec <krava255@gmail.com>
2022-05-19 11:26:22 +0300

    Add Product items

--- src/components/PLP/ProductСard.js
+++ src/components/PLP/ProductСard.js
@@ -6,27 +6,67 @@ import switer from '../../images/Switer.jpg'
 class ProductСard extends PureComponent {
     render() {
         return <div>
-            <div className={styles.card}>
-                <ul>
-                    <li>
-                        <Link to="/PDP">
-                            <div>
-                                <img src={switer} alt="switer" />
-                            </div>
-                            <div>
-                                <h3>Apollo Running Short</h3>
-                                <p>$50.00</p>
-                            </div>
-                        </Link>
-                    </li>
-                </ul>
-            </div>
             <div>
-                <ul>
-                    <li>
+                <ul className={styles.card} >
+                    <li className={styles.item}>
+                        <Link className={styles.link} to="/PDP">
+                            <div>
+                                <img src={switer} alt="switer" width="354" />
+                            </div>
+                            <div>
+                                <h3 className={styles.text}>Apollo Running Short</h3>
+                                <p>$50.00</p>
+                            </div>
+                        </Link>
+                    </li>
+                    <li className={styles.item}>
                         <Link to="/PDP">
                             <div>
-                                <img src={switer} alt="switer" />
+                                <img src={switer} alt="switer" className={styles.img} />
+                            </div>
+                            <div>
+                                <h3>Apollo Running Short</h3>
+                                <p>$50.00</p>
+                            </div>
+                        </Link>
+                    </li>
+                    <li className={styles.item}>
+                        <Link to="/PDP">
+                            <div>
+                                <img src={switer} alt="switer" className={styles.img} />
+                            </div>
+                            <div>
+                                <h3>Apollo Running Short</h3>
+                                <p>$50.00</p>
+                            </div>
+                        </Link>
+                    </li>
+                    <li className={styles.item}>
+                        <Link to="/PDP">
+                            <div>
+                                <img src={switer} alt="switer" className={styles.img} />
+                            </div>
+                            <div>
+                                <h3>Apollo Running Short</h3>
+                                <p>$50.00</p>
+                            </div>
+                        </Link>
+                    </li>
+                    <li className={styles.item}>
+                        <Link to="/PDP">
+                            <div>
+                                <img src={switer} alt="switer" className={styles.img} />
+                            </div>
+                            <div>
+                                <h3>Apollo Running Short</h3>
+                                <p>$50.00</p>
+                            </div>
+                        </Link>
+                    </li>
+                    <li className={styles.item}>
+                        <Link to="/PDP">
+                            <div>
+                                <img src={switer} alt="switer" className={styles.img} />
                             </div>
                             <div>
                                 <h3>Apollo Running Short</h3>
@@ -36,7 +76,6 @@ class ProductСard extends PureComponent {
                     </li>
                 </ul>
             </div>
-
         </div>
     }
 
