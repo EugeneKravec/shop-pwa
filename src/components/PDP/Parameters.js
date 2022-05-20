f78c626d0d490b3c9482f0500e81a20c8dc8f89d
Eugene Kravec <krava255@gmail.com>
2022-05-20 23:24:08 +0300

    add Parameters Component

--- src/components/PDP/Parameters.js
+++ src/components/PDP/Parameters.js
@@ -9,6 +9,7 @@ class Parameters extends PureComponent {
             <h2 className={styles.text}>Apollo</h2>
             <h2 className={styles.secondText}>Running Short</h2>
             <div className={styles.filterContainer}>
+                <h2 className={styles.text}>SIZE:</h2>
                 <ul className={styles.filterList}>
                     <li className={styles.buttonItem}>
                         <button className={styles.buttonFilter} type="button">XS</button>
@@ -24,7 +25,31 @@ class Parameters extends PureComponent {
                     </li>
                 </ul>
             </div>
-
+            <div className={styles.filterContainer}>
+                <h2 className={styles.text}>COLOR:</h2>
+                <ul className={styles.filterList}>
+                    <li className={styles.buttonItem}>
+                        <button className={styles.buttonFilter} type="button"></button>
+                    </li>
+                    <li className={styles.buttonItem}>
+                        <button className={styles.buttonFilter} type="button"></button>
+                    </li>
+                    <li className={styles.buttonItem}>
+                        <button className={styles.buttonFilter} type="button"></button>
+                    </li>
+                    <li className={styles.buttonItem}>
+                        <button className={styles.buttonFilter} type="button"></button>
+                    </li>
+                </ul>
+            </div>
+            <h2 className={styles.text}>PRICE</h2>
+            <h2 className={styles.text}>$50.00</h2>
+            <button>ADD TO CART</button>
+            <p>
+                Find stunning women's cocktail dresses and party dresses.
+                Stand out in lace and metallic cocktail dresses and party
+                dresses from all your favorite brands.
+            </p>
         </div>
     }
 }
--- src/components/PLP/ProductСard.js
+++ src/components/PLP/ProductСard.js
@@ -2,6 +2,9 @@ import { PureComponent } from "react";
 import { Link } from "react-router-dom";
 import styles from './ProductСard.module.css'
 import switer from '../../images/Switer.jpg'
+import pinkSwiter from '../../images/pinkSwiter.jpg'
+import whiteSwiter from '../../images/whiteSwiter.jpg'
+import bag from '../../images/bag.jpg'
 
 class ProductСard extends PureComponent {
     render() {
@@ -22,7 +25,32 @@ class ProductСard extends PureComponent {
                     <li className={styles.item}>
                         <Link className={styles.link} to="/PDP">
                             <div>
-                                <img src={switer} alt="switer" width="354" />
+                                <img src={pinkSwiter} alt="pinkSwiter" width="354" />
+                            </div>
+                            <div>
+                                <h3 className={styles.text}>Apollo Running Short</h3>
+                                <p className={styles.price}>$50.00</p>
+                            </div>
+                        </Link>
+                    </li>
+                    <li className={styles.item}>
+                        <Link className={styles.link} to="/PDP">
+                            <div>
+                                <img src={whiteSwiter} alt="switer" width="354" />
+                                <div className={styles.overlay}>
+                                    <p className={styles.text}>OUT OF STOCK</p>
+                                </div>
+                            </div>
+                            <div>
+                                <h3 className={styles.text}>Apollo Running Short</h3>
+                                <p className={styles.price}>$50.00</p>
+                            </div>
+                        </Link>
+                    </li>
+                    <li className={styles.item}>
+                        <Link className={styles.link} to="/PDP">
+                            <div>
+                                <img src={bag} alt="bag" width="354" />
                             </div>
                             <div>
                                 <h3 className={styles.text}>Apollo Running Short</h3>
@@ -44,29 +72,7 @@ class ProductСard extends PureComponent {
                     <li className={styles.item}>
                         <Link className={styles.link} to="/PDP">
                             <div>
-                                <img src={switer} alt="switer" width="354" />
-                            </div>
-                            <div>
-                                <h3 className={styles.text}>Apollo Running Short</h3>
-                                <p className={styles.price}>$50.00</p>
-                            </div>
-                        </Link>
-                    </li>
-                    <li className={styles.item}>
-                        <Link className={styles.link} to="/PDP">
-                            <div>
-                                <img src={switer} alt="switer" width="354" />
-                            </div>
-                            <div>
-                                <h3 className={styles.text}>Apollo Running Short</h3>
-                                <p className={styles.price}>$50.00</p>
-                            </div>
-                        </Link>
-                    </li>
-                    <li className={styles.item}>
-                        <Link className={styles.link} to="/PDP">
-                            <div>
-                                <img src={switer} alt="switer" width="354" />
+                                <img src={whiteSwiter} alt="whiteSwiter" width="354" />
                             </div>
                             <div>
                                 <h3 className={styles.text}>Apollo Running Short</h3>
